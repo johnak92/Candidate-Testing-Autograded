e30e70652f51521f05f2424f2ceff1e8e4943b2f
Jak W <kussman.johna@gmail.com>
2024-05-20 19:35:10 -0500

    Candidate Test Part 2 Complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,6 +12,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
+<<<<<<< HEAD
 
 
 /*if(question === correctAnswer){
@@ -23,29 +24,55 @@ let candidateAnswer = "";
 }*/
   
 
+=======
+>>>>>>> 5a0a213 (Part 2 Complete)
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride", 
+  "true", 
+  "40", 
+  "Trajectory", 
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question("Candidate Name: ");
+<<<<<<< HEAD
 }
+=======
+} 
+>>>>>>> 5a0a213 (Part 2 Complete)
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question);
+<<<<<<< HEAD
 
 }
 
+=======
+  }  
+>>>>>>> 5a0a213 (Part 2 Complete)
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+if(correctAnswer === candidateAnswer){
+  console.log("That's the right answer!");
+} else {
+  console.log("Wrong answer!");
+}
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
